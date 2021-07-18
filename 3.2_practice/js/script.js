70cc9bef6eb6393123a5f1ab2b026c7d77f164b5
Andrey Zykov <andrey.zykov2000@yandex.ru>
2021-07-18 03:42:50 +0300

    3.2_practice

---
+++ 3.2_practice/js/script.js
@@ -0,0 +1,8 @@
+const Page = document.querySelector(".page");
+Page.style.backgroundColor = "ghostwhite";
+const Name = document.querySelector(".name");
+Name.innerHTML = "<h1>DJ Rashad</h1>";
+const Img = document.querySelector("img");
+Img.setAttribute("src", "https://www.chicagotribune.com/resizer/rerU3hB9-ZUPBsDz4VnFJWLiGLA=/1200x0/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/VCPD6CUIYFDKBIG5ZAUYT4BE6E.jpg");
+const Bio = document.querySelector(".short-bio");
+Bio.className += " animated";

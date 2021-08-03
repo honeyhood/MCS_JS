9d965c8d0dc7b1d234d6665f8b8cec6bd232f1d9
Andrey Zykov <andrey.zykov2000@yandex.ru>
2021-08-03 23:54:55 +0300

    6.1_practice

--- 6.1_practice/js/script.js
+++ 6.1_practice/js/script.js
@@ -1,23 +1,17 @@
-const APIKey = "8493b1efa047701cfaeb2f79f988147b";
-const city = document.querySelector(".city").value;
-const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIKey;
-const result = document.querySelector(".temperature");
-
 const form = document.forms[0];
 
 form.onsubmit = e => {
 	e.preventDefault();
-	city = document.querySelector(".city").value;
-	url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIKey;
+	const APIKey = "8493b1efa047701cfaeb2f79f988147b";
+	let city = document.querySelector(".city").value;
+	let url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIKey;
+	let temperature = document.querySelector(".temperature");
+	let humidity = document.querySelector(".humidity");
 	let xhr = new XMLHttpRequest();
 	xhr.open("GET", url, false);
 	xhr.send();
 	let DATA = JSON.parse(xhr.responseText);
-	result.innerHTML = DATA.main.temp - 273;
+	console.log(DATA);
+	temperature.innerHTML = Math.round(DATA.main.temp) - 273;
+	humidity.innerHTML = DATA.main.humidity;
 }
-
-
-/*form.onsubmit = function(e){
-	e.preventDefault();
-	result.innerHTML = DATA.main.temp - 273;
-}*/
